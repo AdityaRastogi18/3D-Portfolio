07596aa8c68917b47eb3a857f870b08a1ab1f7c0
Aditya Rastogi <deathracer18@Adityas-Laptop.local>
2024-10-25 13:10:09 +0530

    minor update

--- src/constants/index.js
+++ src/constants/index.js
@@ -118,7 +118,7 @@ const experiences = [
     company_name: "Being The Parent",
     icon: btp,
     iconBg: "#383E56",
-    date: "May 2023 - Present",
+    date: "May 2023 - May 2024",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
