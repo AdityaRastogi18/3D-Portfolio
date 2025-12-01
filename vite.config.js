007167ec7eef617b1a95124e7f9d09135e382dc9
Aditya Rastogi <73739276+AdityaRastogi18@users.noreply.github.com>
2025-12-01 17:06:02 +0530

    Remove Webflow proxy configuration from Vite
    Remove server proxy configuration for Webflow.

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/': {
-        target: 'https://webflow.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/(.*)/, '/$1'),
-      }
-    }
-  }
+  
 })
